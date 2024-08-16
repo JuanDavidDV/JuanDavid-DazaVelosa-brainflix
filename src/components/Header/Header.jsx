4546d15af93710d3b0bd2145bf5350ac05630946
JuanDavidDV <juandadazav@gmail.com>
2024-08-15 20:37:22 -0400

    fixed main HTML title and started Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = prop => {
+    return (
+        <header className="header">
+            <div className="header__container">
+                
+            </div>
+
+        </header>
+    );
+};
+
+export default Header;
