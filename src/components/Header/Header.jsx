4b0bad1bd8d9120b424b6ba5fe6fe74e1a60b66a
JuanDavidDV <juandadazav@gmail.com>
2024-08-15 22:23:54 -0400

    logo inserted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
+import brainFlixLogo from "../../assets/logo/BrainFlix-logo.svg"
+
 const Header = prop => {
     return (
         <header className="header">
             <div className="header__container">
-                
+                <a className="header__container__logo__link" href="../../index.html">
+                    <img className="header__container__logo" src={brainFlixLogo} alt="brainFlix-logo"/>
+                </a>
             </div>
 
         </header>
