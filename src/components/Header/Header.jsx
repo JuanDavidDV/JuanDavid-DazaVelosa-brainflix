5bd09525ebf1db6f5ca0180c18fc78c29542749c
JuanDavidDV <juandadazav@gmail.com>
2024-08-17 10:03:54 -0400

    avatar header aded

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.scss";
-import brainFlixLogo from "../../assets/logo/BrainFlix-logo.svg"
-import search from "../../assets/icons/search.svg"
+import brainFlixLogo from "../../assets/logo/BrainFlix-logo.svg";
+import search from "../../assets/icons/search.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
 
 const Header = prop => {
     return (
@@ -9,9 +10,13 @@ const Header = prop => {
                 <a className="header__container__logo__link" href="../../index.html">
                     <img className="header__container__logo" src={brainFlixLogo} alt="brainFlix-logo"/>
                 </a>
-                <div className="header__container__search-bar">
-                    <img className="header__container__search-bar--icon" src={search} />
-                    <input className="header__container__search-bar--field" type="text" name="search" placeholder="Search"/>
+                <div className="header__container__box">
+                    <div className="header__container__box__search-bar">
+                        <img className="header__container__box__search-bar--icon" src={search} />
+                        <input className="header__container__box__search-bar--field" type="text" name="search" placeholder="Search"/>
+                    </div>
+                    <img className="header__container__box__avatar" src={avatar}/>
+
                 </div>
                 
             </div>
