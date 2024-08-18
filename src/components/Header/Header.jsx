456502be3137b2b921d1e0d32f2385f3f11193b7
JuanDavidDV <juandadazav@gmail.com>
2024-08-17 21:15:13 -0400

    header for mobile, tablet and desktop has been completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,24 +11,23 @@ const Header = () => {
                 <a className="header__container__logo__link" href="../../index.html">
                     <img className="header__container__logo" src={brainFlixLogo} alt="brainFlix-logo"/>
                 </a>
-                <div className="header__container__box">
-                    <div className="header__container__box__search-bar">
-                        <img className="header__container__box__search-bar--icon" src={search} alt="search-icon"/>
-                        <input className="header__container__box__search-bar--field" type="text" name="search" placeholder="Search"/>
+                <div className="header__container__wrapper">
+                    <div className="header__container__wrapper__box">
+                        <div className="header__container__wrapper__box__search-bar">
+                            <img className="header__container__wrapper__box__search-bar--icon" src={search} alt="search-icon"/>
+                            <input className="header__container__wrapper__box__search-bar--field" type="text" name="search" placeholder="Search"/>
+                        </div>
+                        <img className="header__container__wrapper__box__avatar" src={avatar} alt="avatar"/>
                     </div>
-                    <img className="header__container__box__avatar" src={avatar} alt="avatar"/>
+                    <div className="header__container__wrapper__button"> 
+                        <button className="header__container__wrapper__button__upload--button">
+                            <img className="header__container__wrapper__button__upload--icon" src={upload} alt="upload-icon"/>
+                            UPLOAD
+                        </button>
+                        <img className="header__container__wrapper__box__avatar--tablet" src={avatar} alt="avatar"/>
+                    </div>
                 </div>
-                <div className="header__container__button">
-                    
-                    <button className="header__container__button__upload--button">
-                        <img className="header__container__button__upload--icon" src={upload} alt="upload-icon"/>
-                        UPLOAD
-                    </button>
-                </div>
-                
-                
             </div>
-
         </header>
     );
 };
