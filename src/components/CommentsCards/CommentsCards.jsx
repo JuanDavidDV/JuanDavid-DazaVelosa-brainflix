752df9efb5365ce6f025a529bf5ba10ca4315948
JuanDavidDV <juandadazav@gmail.com>
2024-08-19 22:02:29 -0400

    fixed class name for comment form section

--- src/components/CommentsCards/CommentsCards.jsx
+++ src/components/CommentsCards/CommentsCards.jsx
@@ -1,13 +1,15 @@
 import "./CommentsCards.scss";
-import avatar from "../../assets/images/Mohan-muruge.jpg";
 
 const CommentsCards = ({ name, timestamp, comment }) => {
     return (
         <article className="comments-cards">
-            <div className="comments-cards__avatar"></div>
-            <div className="">
+            <div className="comments-cards__container">
+                <div className="comments-cards__container__avatar"></div>
+                <div className="">
 
+                </div>
             </div>
+            
         </article>
     )
 }
--- src/components/CommentsSectionForm/CommentsSectionForm.jsx
+++ src/components/CommentsSectionForm/CommentsSectionForm.jsx
@@ -4,20 +4,20 @@ import addCommentIcon from "../../assets/icons/add_comment.svg";
 
 const CommentsSectionForm = (props) => {
     return (
-        <section className="comment">
-            <div className="comment__container">
-                <h3 className="comment__container--total-comments">{props.heroVideoData.comments.length} Comments</h3>
-            <form className="comment__container__form">
-                <img className="comment__container__form__avatar" src={avatar}/>
-                <div className="comment__container__form__wrapper">
-                    <div className="comment__container__form__wrapper__ui">
-                        <label className="comment__container__form__wrapper__ui__label-comment" htmlFor="inputUserComment">
+        <section className="comment-input">
+            <div className="comment-input__container">
+                <h3 className="comment-input__container--total-comments">{props.heroVideoData.comments.length} Comments</h3>
+            <form className="comment-input__container__form">
+                <img className="comment-input__container__form__avatar" src={avatar}/>
+                <div className="comment-input__container__form__wrapper">
+                    <div className="comment-input__container__form__wrapper__ui">
+                        <label className="comment-input__container__form__wrapper__ui__label-comment" htmlFor="inputUserComment">
                                 JOIN THE CONVERSATION
                         </label>
-                        <textarea className="comment__container__form__wrapper__ui__input-comment" name="inputUserComment" placeholder="Add a new comment"/>
+                        <textarea className="comment-input__container__form__wrapper__ui__input-comment" name="inputUserComment" placeholder="Add a new comment"/>
                     </div>  
-                    <button className="comment__container__form__wrapper__button">
-                        <img className="comment__container__form__wrapper__button--icon" src={addCommentIcon} alt="comment-icon"/>
+                    <button className="comment-input__container__form__wrapper__button">
+                        <img className="comment-input__container__form__wrapper__button--icon" src={addCommentIcon} alt="comment-icon"/>
                         COMMENT
                     </button>
                 </div>
