07529aeff7d606ba6d28cb610a8894fe8c44f52f
JuanDavidDV <juandadazav@gmail.com>
2024-08-20 22:33:57 -0400

    next video mobile display completed

--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -1,22 +1,23 @@
 import NextVideosCards from "../NextVideosCards/NextVideosCards";
+import "./NextVideos.scss"
 
 const NextVideos = ({ videos }) => {
     console.log(videos);
     return (
         <section className="next-videos">
             <div className="next-videos__container">
-                <h2>NEXT VIDEOS</h2>
-            <ul className="next-videos__container__content">
-                {videos.map((video) => (
-                    <li className="next-videos__container__content__list" key={video.id}>
-                        <NextVideosCards 
-                            nextVideoImage = {video.image}
-                            nextVideoTitle = {video.title}
-                            nextVideoChannel = {video.channel}
-                        />
-                    </li>
-                ))}
-            </ul>
+                <p className="next-videos__container__section-title">NEXT VIDEOS</p>
+                <ul className="next-videos__container__content">
+                    {videos.map((video) => (
+                        <li className="next-videos__container__content__list" key={video.id}>
+                            <NextVideosCards 
+                                nextVideoImage = {video.image}
+                                nextVideoTitle = {video.title}
+                                nextVideoChannel = {video.channel}
+                            />
+                        </li>
+                    ))}
+                </ul>
             </div>
         </section>
     );
