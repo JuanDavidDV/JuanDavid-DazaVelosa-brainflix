6291b7aa19ac77fe3a5d075dec17d321531296f1
JuanDavidDV <juandadazav@gmail.com>
2024-08-24 17:58:51 -0400

    avatar placeholder comment added

--- src/components/CommentsCards/CommentsCards.jsx
+++ src/components/CommentsCards/CommentsCards.jsx
@@ -4,7 +4,7 @@ import { timeAgo } from "../HeroVideoDetails/HeroVideoDetails";
 const CommentsCards = ({ name, timestamp, comment }) => {
     return (
         <article className="comments-cards__container">
-            <div className="comments-cards__container__avatar"></div>   {/*Created to add gray default avatar*/}
+            <div className="comments-cards__container__avatar"></div>   {/*Created to add gray default avatar placeholder*/}
             <div className="comments-cards__container__user-info">
                 <div className="comments-cards__container__user-info__details">
                     <h3 className="comments-cards__container__user-info__details--name">{name}</h3>
