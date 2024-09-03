6c1f8c498e4e8ecdd68c015a06e3532056e824e3
JuanDavidDV <juandadazav@gmail.com>
2024-09-03 08:46:58 -0400

    not found page layout and style completed

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,11 @@
+import "./NotFoundPage.scss"
+
 const NotFoundPage = () => {
     return (
         <main className="not-found-page">
             <div className="not-found-page__container">
                 <h1 className="not-found-page__container--title">Oooops, something went wrong 😲</h1>
-                <p className="not-found-page__container--text">The page you are trying to reach does not exist...</p>
+                <p className="not-found-page__container--text">The page you are trying to reach does not exists...</p>
                 <button className="not-found-page__container__button">Return Home Page</button>
             </div>
         </main>
