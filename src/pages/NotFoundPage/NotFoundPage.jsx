52d2e4b3c81f830332fb99817d0e271fdb0bad6d
JuanDavidDV <juandadazav@gmail.com>
2024-09-03 08:58:26 -0400

    not found page finalized

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,12 +1,18 @@
-import "./NotFoundPage.scss"
+import { useNavigate } from "react-router-dom";
+import "./NotFoundPage.scss";
 
 const NotFoundPage = () => {
+    const navigate = useNavigate();
+    const hundleClick = () => {
+        navigate("/");
+    }
+
     return (
         <main className="not-found-page">
             <div className="not-found-page__container">
                 <h1 className="not-found-page__container--title">Oooops, something went wrong 😲</h1>
                 <p className="not-found-page__container--text">The page you are trying to reach does not exists...</p>
-                <button className="not-found-page__container__button">Return Home Page</button>
+                <button className="not-found-page__container__button" onClick={hundleClick}>Return Home Page</button>
             </div>
         </main>
     )
