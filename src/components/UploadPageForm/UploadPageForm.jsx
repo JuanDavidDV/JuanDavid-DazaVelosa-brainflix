b7f0d3f017f7df4c77637ff26b04501e6d0ff4ee
JuanDavidDV <juandadazav@gmail.com>
2024-09-29 22:03:17 -0400

    image uploaded from user completed

--- src/components/UploadPageForm/UploadPageForm.jsx
+++ src/components/UploadPageForm/UploadPageForm.jsx
@@ -1,68 +1,77 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useRef } from "react";
+import { useState } from "react";
 import './UploadPageForm.scss';
 import uploadVideoPreview from '../../assets/images/Upload-video-preview.jpg';
 import publishIcon from '../../assets/icons/publish.svg'
 import VideoApi from "../../utilsJs/utilsApi";
 
+const DEFAULT_FORM_STATE = {
+    title: '',
+    imageUrl: '',
+    description: ''
+}
 
 const UploadPageForm = () => {
-
-    const formRef = useRef();
+    const [formDetails, setFormDetails] = useState(DEFAULT_FORM_STATE);
     const navigate = useNavigate();
+
+    const convertImage = e => {
+        const file = e.target.files[0];
+        if(file) {
+            const url = URL.createObjectURL(file);
+            updateFormDetails('imageUrl', url)
+        }
+    };
+
+    const updateFormDetails = (prop, value) => {
+        setFormDetails(prevDetails => ({
+            ...prevDetails,
+            [prop]: value
+        }));
+    }
 
     const uploadSubmit = async event => {
         event.preventDefault();
-
-        //Add new video details from front-end user input to the back-end server and then updates the state with the response
-        const title = formRef.current.videoUploadTitle.value;
-        const image = formRef.current.videoUploadImage.value;
-        const description = formRef.current.videoUploadDescription.value;    
-        
-        const newUploadVideo = {
-            title: title,
-            image: image, 
-            description: description
-        }
          
-        if(title && description) {
+        if(formDetails.title && formDetails.description) {
             const newVideo = new VideoApi();
-            const newVideoPost = await newVideo.postNewVideo(newUploadVideo);
-            event.target.reset();   //Clears input fields after submitting a new comment
+            await newVideo.postNewVideo(formDetails);
+            setFormDetails(DEFAULT_FORM_STATE); // reset form to default state
             alert("Video has been successfully uploaded!");
             navigate("/");
         }
 
-        if(title.length === 0) {
-            formRef.current.videoUploadTitle.classList.add("upload-form__container__top__input-box__video-title__input--invalid");
+        if(formDetails.title.length === 0) {
+            event.target.videoUploadTitle.classList.add("upload-form__container__top__input-box__video-title__input--invalid");
         } 
-        if  (description.length === 0) {
-            formRef.current.videoUploadDescription.classList.add("upload-form__container__top__input-box__video-description__input--invalid");
+        if  (formDetails.description.length === 0) {
+            event.target.videoUploadDescription.classList.add("upload-form__container__top__input-box__video-description__input--invalid");
         }
     }
 
+
     return (
-        <form className="upload-form" onSubmit={uploadSubmit} ref={formRef}>
+        <form className="upload-form" onSubmit={uploadSubmit}>
             <div className="upload-form__container">
                 <div className="upload-form__container__top">
                     <div className="upload-form__container__top__image-box">
                         <label className="upload-form__container__top__image-box__label" htmlFor="videoThumbnailUpload">VIDEO THUMBNAIL</label>
                         <div className="upload-form__container__top__image-box__sub-container">
-                            <img className="upload-form__container__top__image-box__sub-container--video-thumbnail" src={uploadVideoPreview} alt="videoThumbnailUpload"/>
+                            <img className="upload-form__container__top__image-box__sub-container--video-thumbnail" src={formDetails.imageUrl || uploadVideoPreview} alt="videoThumbnailUpload"/>
                         </div> 
                     </div>
                     <div className="upload-form__container__top__input-box">
                         <div className="upload-form__container__top__input-box__video-title">
                             <label className="upload-form__container__top__input-box__video-title__label" htmlFor="videoUploadTitle">TITLE YOUR VIDEO</label>
-                            <input className="upload-form__container__top__input-box__video-title__input" name="videoUploadTitle" type="text" placeholder="Add a title to your video" id="videoUploadTitle"/>
+                            <input className="upload-form__container__top__input-box__video-title__input" name="videoUploadTitle" onChange={(e) => updateFormDetails('title', e.target.value)} value={formDetails.title} type="text" placeholder="Add a title to your video" id="videoUploadTitle"/>
                         </div>
                         <div className="upload-form__container__top__input-box__video-description">
                             <label className="upload-form__container__top__input-box__video-description__label" htmlFor="videoUploadDescription">ADD A VIDEO DESCRIPTION</label>
-                            <textarea className="upload-form__container__top__input-box__video-description__input" name="videoUploadDescription" placeholder="Add a description to your video" type="text" id="videoUploadDescription"/>
+                            <textarea className="upload-form__container__top__input-box__video-description__input" name="videoUploadDescription" onChange={(e) => updateFormDetails('description', e.target.value)} value={formDetails.description} placeholder="Add a description to your video" type="text" id="videoUploadDescription"/>
                         </div>
                         <div className="upload-form__container__top__input-box__video-image">
                             <label className="upload-form__container__top__input-box__video-image__label" htmlFor="videoUploadChannel">VIDEO IMAGE (optional)</label>
-                            <input type="file" className="upload-form__container__top__input-box__video-image--file" id="videoUploadImage" name="videoUploadImage"></input>                 
+                            <input className="upload-form__container__top__input-box__video-image--file" type="file" onChange={convertImage} id="videoUploadImage" name="videoUploadImage"/>                 
                         </div>
                     </div>
                 </div>   
